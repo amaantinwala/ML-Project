f31660d5d9ac50cfac6cfb50d596b6a555fd73c3
amaantinwala <50973896+amaantinwala@users.noreply.github.com>
2024-03-01 11:23:41 +0530

    Cleared All Files

--- index.js
+++
@@ -1,7 +0,0 @@
-// Function to open the print dialog
-function openPrintDialog() {
-    window.print();
-}
-
-// Add event listener to the "Generate PDF Report" button
-document.getElementById("generate-report-btn").addEventListener("click", openPrintDialog);
